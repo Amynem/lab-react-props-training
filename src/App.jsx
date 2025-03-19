120cf4a8bf6f5ec46a9cc7c8060f551cab8e44fb
Amynem <amy.lauren98@hotmail.com>
2025-03-19 22:26:29 +0100

    labb

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,8 @@
-import IdCard from "./assets/components/idCard/idCard";
-import Greetings from "./assets/components/greetings/greetings";
-import Random from "./assets/components/random/random";
-import BoxColor from "./assets/components/boxColor/boxColor";
-import CreditCard from "./assets/components/creditCard/creditCard";
-
-
+import BoxColor from "./components/box-color/box-color";
+import CreditCard from "./components/credit-card/credit-card";
+import Greetings from "./components/greetings/greetings";
+import IdCard from "./components/id-card/id-card";
+import Random from "./components/random/random";
 
 function App() {
   return (
@@ -20,24 +18,24 @@ function App() {
       />
       <IdCard
         className="mb-2"
-        lastName='Delores '
-        firstName='Obrien'
-        gender='female'
-        height={172}
-        birth={new Date("1988-05-11")}
-        picture="https://randomuser.me/api/portraits/women/44.jpg"
+        lastName='Doe'
+        firstName='John'
+        gender='male'
+        height={178}
+        birth={new Date("1992-07-14")}
+        picture="https://randomuser.me/api/portraits/men/44.jpg"
       />
 
-      <Greetings className="mb-1">Esther</Greetings>
-      <Greetings className="mb-1" lang="fr">Julia y Sofia</Greetings>
+      <Greetings className="mb-1">Carlos</Greetings>
+      <Greetings className="mb-1" lang="fr">Juli</Greetings>
 
       <Random className="mb-1" min={0} max={50} />
-      <Random className="mb-1" min={1} max={6} />
-      <Random className="mb-1" min={2} max={100} />
+      <Random className="mb-1" min={5} max={6} />
+      <Random className="mb-1" min={3} max={10} />
 
       <BoxColor className="mb-1" r={255} g={0} b={0} />
-      <BoxColor className="mb-1" r={128} g={255} b={0} />
-      <BoxColor className="mb-1" r={0} g={255} b={255} />
+      <BoxColor className="mb-1" r={0} g={0} b={255} />
+      <BoxColor className="mb-1" r={0} g={255} b={0} />
 
       <CreditCard 
         className="mb-1"
@@ -46,8 +44,8 @@ function App() {
           number: "0123456789016984",
           expirationMonth: 12,
           expirationYear: 2019,
-          bank: "CaixaBank",
-          owner: "Esther Lopez Perez",
+          bank: "Name of the Bank",
+          owner: "Firstname Lastname",
         }}
         bgColor="#ddbb55"
         color="white" 
